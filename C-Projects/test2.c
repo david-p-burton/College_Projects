b7f3015b9c35e6feecd8b378575407a7723c811d
David Burton <David@davids-mbp-2.ict.ad.dit.ie>
2016-10-24 13:17:39 +0100

    Initial test

---
+++ C-Projects/test2.c
@@ -0,0 +1,12 @@
+/*
+Author: David Burton
+Date; 24/Oct/16
+Purpose of code; Just to test out github, git on my machine, branching and commits
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World!");
+}
